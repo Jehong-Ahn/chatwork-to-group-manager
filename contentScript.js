7baa932a91d3aa5937d9ba42d0c0feb023054f73
Jehong Ahn <toonitw@gmail.com>
2018-10-01 12:21:23 +0900

    인덴트 조정

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,7 @@
 const PresetManager = ()=>{
 
   if (document.getElementById("_presetList")) 
-      document.getElementById("_presetList").remove();
+    document.getElementById("_presetList").remove();
 
   let toBtn = document.getElementById("_to");
   let toList = document.getElementById("_toList");
@@ -12,7 +12,7 @@ const PresetManager = ()=>{
 
   let data = {};
   chrome.storage.local.get(['data'], (res) => {
-      data = res.data || {};
+    data = res.data || {};
   });
 
   let container = document.createElement("div");
@@ -50,37 +50,37 @@ const PresetManager = ()=>{
   btnAdd.textContent = "Add";
   btnAdd.addEventListener("click", async () => {
       
-      let content = chat.value.trim();
-      if (!content) {
-          memberList.style.backgroundColor = "yellow";
-          setTimeout(() => {
-              memberList.style.backgroundColor = "inherit";
-          }, 1000);
-          return false;
-      }
+    let content = chat.value.trim();
+    if (!content) {
+      memberList.style.backgroundColor = "yellow";
+      setTimeout(() => {
+        memberList.style.backgroundColor = "inherit";
+      }, 1000);
+      return false;
+    }
 
-      let name = inputName.value.trim();
-      if (!name) return inputName.focus();
+    let name = inputName.value.trim();
+    if (!name) return inputName.focus();
 
-      let array = content.split("\n")
-          .filter(line=>line.substr(0,4)==="[To:")
-          .map(line=>/To:(\d+)/.exec(line)[1]);
-      if (!array.length) return false;
+    let array = content.split("\n")
+      .filter(line=>line.substr(0,4)==="[To:")
+      .map(line=>/To:(\d+)/.exec(line)[1]);
+    if (!array.length) return false;
 
-      let row = [
-          Date.now().toString(),
-          name,
-          array
-      ];
-      console.log(data);
-      data[room].push(row);
-      let item = renderItem(row);
-      list.appendChild(item);
-      console.log("data", data);
-      await save();
+    let row = [
+      Date.now().toString(),
+      name,
+      array
+    ];
+    console.log(data);
+    data[room].push(row);
+    let item = renderItem(row);
+    list.appendChild(item);
+    console.log("data", data);
+    await save();
 
-      inputName.value = "";
-      item.scrollIntoView();
+    inputName.value = "";
+    item.scrollIntoView();
   });
   form.appendChild(btnAdd);
   
@@ -91,105 +91,105 @@ const PresetManager = ()=>{
 
   const renderItem = (row, i) => {
 
-      let item = document.createElement("li");
-      item.className = "tooltipList__item";
-      item.setAttribute("key", row[0]);
-      item.addEventListener("click", (e)=>{
-          if (e.target.nodeName==="BUTTON") return false;
-          chat.value = generateMessage(row[2]) + "\n";
-          toBtn.click();
-          chat.focus();
-      });
+    let item = document.createElement("li");
+    item.className = "tooltipList__item";
+    item.setAttribute("key", row[0]);
+    item.addEventListener("click", (e)=>{
+      if (e.target.nodeName==="BUTTON") return false;
+      chat.value = generateMessage(row[2]) + "\n";
+      toBtn.click();
+      chat.focus();
+    });
 
-      let label = document.createElement("p");
-      label.className = "autotrim";
-      label.style = "width: 70px; margin-right: 5px;";
-      label.textContent = row[1];
-      item.appendChild(label);
+    let label = document.createElement("p");
+    label.className = "autotrim";
+    label.style = "width: 70px; margin-right: 5px;";
+    label.textContent = row[1];
+    item.appendChild(label);
 
-      let avatars = document.createElement("div");
-      avatars.className = "avatars autotrim";
-      avatars.style = "display: inline-block; width: 73px; margin-right: 5px;";
-      item.appendChild(avatars);
+    let avatars = document.createElement("div");
+    avatars.className = "avatars autotrim";
+    avatars.style = "display: inline-block; width: 73px; margin-right: 5px;";
+    item.appendChild(avatars);
 
-      copyAvatar(row[2], avatars);
+    copyAvatar(row[2], avatars);
 
-      let btn = document.createElement("button");
-      btn.textContent = "X";
-      btn.style = "visibility: hidden; cursor: pointer; font-size: 11px; border: 0;";
-      btn.addEventListener("click", async (e)=>{
-          let key = e.target.parentNode.getAttribute("key");
-          data[room].splice(
-              data[room].findIndex((row)=>row[0]===key), 1
-          );
-          e.target.parentNode.remove();
-          console.log('data', data[room]);
-          await save();
-      });
-      item.appendChild(btn);
+    let btn = document.createElement("button");
+    btn.textContent = "X";
+    btn.style = "visibility: hidden; cursor: pointer; font-size: 11px; border: 0;";
+    btn.addEventListener("click", async (e)=>{
+      let key = e.target.parentNode.getAttribute("key");
+      data[room].splice(
+        data[room].findIndex((row)=>row[0]===key), 1
+      );
+      e.target.parentNode.remove();
+      console.log('data', data[room]);
+      await save();
+    });
+    item.appendChild(btn);
 
-      item.addEventListener("mouseenter", ()=>{ btn.style.visibility = "visible"; });
-      item.addEventListener("mouseleave", ()=>{ btn.style.visibility = "hidden"; });
+    item.addEventListener("mouseenter", ()=>{ btn.style.visibility = "visible"; });
+    item.addEventListener("mouseleave", ()=>{ btn.style.visibility = "hidden"; });
 
-      return item;
+    return item;
   };
 
   const render = (roomNo) => {
-      
-      console.log("Start to render");
-      room = roomNo;
-      reset();
-      toBtn.click();
-      toBtn.click();
+    
+    console.log("Start to render");
+    room = roomNo;
+    reset();
+    toBtn.click();
+    toBtn.click();
 
-      if (!data[roomNo] || !data[roomNo].length) {
-          data[roomNo] = [];
-          return console.log('no preset data', data);
-      }
+    if (!data[roomNo] || !data[roomNo].length) {
+      data[roomNo] = [];
+      return console.log('no preset data', data);
+    }
 
-      var f = document.createDocumentFragment();
-      data[roomNo].forEach((row, i)=>{
-          f.appendChild(renderItem(row, i));
-      });
-      list.appendChild(f);
+    var f = document.createDocumentFragment();
+    data[roomNo].forEach((row, i)=>{
+      f.appendChild(renderItem(row, i));
+    });
+    list.appendChild(f);
 
   };
 
   const _eachMember = (users, callback) => {
-      for(let i=0; i<members.length; i++) {
-          let m = members[i];
-          let mid = m.getAttribute("data-cwui-lt-value");
-          if (users.slice(0, 3).indexOf(mid)>-1) {
-              callback(mid, m);
-          }
+    for(let i=0; i<members.length; i++) {
+      let m = members[i];
+      let mid = m.getAttribute("data-cwui-lt-value");
+      if (users.slice(0, 3).indexOf(mid)>-1) {
+        callback(mid, m);
       }
+    }
   }
 
   const copyAvatar = (users, item) => {
-      _eachMember(users, (mid, m)=>{
-          let img = m.getElementsByTagName("img")[0].cloneNode(true);
-          img.style = `width: 20px; height: 20px; image-rendering: pixelated;`;
-          item.appendChild(img);            
-      });
+    _eachMember(users, (mid, m)=>{
+      let img = m.getElementsByTagName("img")[0].cloneNode(true);
+      img.style = `width: 20px; height: 20px; image-rendering: pixelated;`;
+      item.appendChild(img);            
+    });
   };
 
   const generateMessage = users => {
-      let lines = [];
-      _eachMember(users, (mid, m)=>{
-          lines.push(`[To:${mid}] ` + m.getElementsByTagName("p")[0].textContent);
-      });
-      return lines.join("\n");
+    let lines = [];
+    _eachMember(users, (mid, m)=>{
+      lines.push(`[To:${mid}] ` + m.getElementsByTagName("p")[0].textContent);
+    });
+    return lines.join("\n");
   };
 
   const reset = () => {
-      console.log('reset');
-      while (list.lastChild) {
-          list.removeChild(list.lastChild);
-      }
+    console.log('reset');
+    while (list.lastChild) {
+      list.removeChild(list.lastChild);
+    }
   };
 
   const save = () => {
-      return chrome.storage.local.set({data});
+    return chrome.storage.local.set({data});
   };
 
   return { data, render };
